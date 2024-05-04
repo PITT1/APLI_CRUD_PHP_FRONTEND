362ec4ae6701a4d7aac3734e790159de62d297a6
PITT1 <petroleo.plta.97@gmail.com>
2024-05-04 19:11:38 -0400

    arreglos menores

--- src/components/SignUpCompo.jsx
+++ src/components/SignUpCompo.jsx
@@ -4,17 +4,17 @@ const SignUpCompo = () => {
     return(
         <div className="py-16 px-8 backdrop-blur-xl rounded-3xl flex flex-col items-center">
             <div className="mb-6 flex flex-col">
-                <input type="text" placeholder="Nombre" className="bg-transparent border-b-2 outline-none text-white text-2xl mb-6 py-2 px-4"/>
-                <input type="text" placeholder="Apellido" className="bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input type="text" placeholder="Nombre" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl mb-6 py-2 px-4"/>
+                <input type="text" placeholder="Apellido" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6">
-                <input type="text" placeholder="Nombre de usuario" className="bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input type="text" placeholder="Nombre de usuario" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6">
-                <input type="email" placeholder="Correo electronico" className="bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input type="email" placeholder="Correo electronico" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6 flex flex-col">
-                <input type="text" placeholder="edad" className="bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4 mb-6"/>
+                <input type="text" placeholder="edad" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4 mb-6"/>
                 <div className="flex justify-between">
                     <label className="cyberpunk-checkbox-label">
                         <input type="checkbox" className="cyberpunk-checkbox" />Masculino
@@ -25,8 +25,8 @@ const SignUpCompo = () => {
                 </div>
             </div>
             <div className="mb-6">
-                <input for="male-selector" type="password" placeholder="Contraseña" className="bg-transparent border-b-2 outline-none text-white text-2xl mr-4 py-2 px-4"/>
-                <input type="password" placeholder="Repetir contraseña" className="bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input for="male-selector" type="password" placeholder="Contraseña" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl mr-4 py-2 px-4"/>
+                <input type="password" placeholder="Repetir contraseña" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="flex flex-col items-center">
                 <button type="button" className="py-3 px-10 mb-6 rounded-full bg-orange-600 hover:bg-orange-500 transition-all text-white text-2xl ">Registrar</button>
