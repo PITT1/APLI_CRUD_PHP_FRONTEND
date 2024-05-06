9ee1e75191d912f34bf15348865b4cc3a8c6edd9
PITT1 <petroleo.plta.97@gmail.com>
2024-05-06 19:59:38 -0400

    capturando el valor de los checkboxes

--- src/components/SignUpCompo.jsx
+++ src/components/SignUpCompo.jsx
@@ -45,17 +45,33 @@ const SignUpCompo = () => {
     return(
         <div className="py-16 px-8 backdrop-blur-xl rounded-3xl flex flex-col items-center">
             <div className="mb-6 flex flex-col">
-                <input type="text" placeholder="Nombre" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl mb-6 py-2 px-4"/>
-                <input type="text" placeholder="Apellido" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input type="text" 
+                       placeholder="Nombre" 
+                       value={name} 
+                       onChange={event => setName(event.target.value)} 
+                       className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl mb-6 py-2 px-4"/>
+                <input type="text" 
+                       placeholder="Apellido" 
+                       value={lastName} 
+                       onChange={event => setLastName(event.target.value)} 
+                       className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6">
                 <input type="text" placeholder="Nombre de usuario" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6">
-                <input type="email" placeholder="Correo electronico" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
+                <input type="email" 
+                       placeholder="Correo electronico" 
+                       value={userName}
+                       onChange={event => setUserName(event.target.value)}
+                       className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4"/>
             </div>
             <div className="mb-6 flex flex-col">
-                <input type="text" placeholder="edad" className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4 mb-6"/>
+                <input type="text"
+                       placeholder="edad"
+                       value={yearsOlds}
+                       onChange={event => setYearsOlds(event.target.value)}
+                       className="placeholder:text-slate-300 bg-transparent border-b-2 outline-none text-white text-2xl py-2 px-4 mb-6"/>
                 <div className="flex justify-between">
                     <label className="cyberpunk-checkbox-label">
                         <input type="checkbox" 
