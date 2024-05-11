6e04329a61849e59ed26d325c31176db0d83d802
PITT1 <petroleo.plta.97@gmail.com>
2024-05-10 20:59:51 -0400

    creando el dashboard para los usuarios

---
+++ src/components/HeaderDashboard.jsx
@@ -0,0 +1,16 @@
+
+
+const HeaderDashboard = () => {
+    return(
+        <div className="flex justify-between py-4 px-8 w-screen h-auto bg-slate-800 text-white">
+            <div> 
+                <h1 className="text-4xl">API-CRUD</h1>
+            </div>
+            <div>
+                <h3 className="text-4xl">username</h3>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderDashboard;
