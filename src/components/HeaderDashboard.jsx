eed07cd408f64fa571ce58d7a8a72e70fc4787d2
PITT1 <petroleo.plta.97@gmail.com>
2024-05-11 22:02:30 -0400

    cambiando el nombre de la pagina principal por ini

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -18,7 +18,7 @@ const HeaderDashboard = () => {
                 <h1 className="text-4xl">API-CRUD</h1>
             </div>
             <ul className="flex gap-4">
-                <li className="text-4xl">{userName}</li>
+                <li className="text-4xl mr-7">Bienvenido {userName}</li>
                 <li><a href="#" className="text-2xl hover:text-orange-500 transition-all">ajustes</a></li>
                 <li><a href="/" className="text-2xl hover:text-orange-500 transition-all">cerrar sesion</a></li>
             </ul>
