6d465bca62f127c4f469bb758c1b8b82d4749746
PITT1 <petroleo.plta.97@gmail.com>
2024-05-15 09:02:03 -0400

    creando inputs para agregar tareas

--- src/components/AddNewTodo.jsx
+++ src/components/AddNewTodo.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 
 const AddNewTodo = () => {
+    const [isClicked, setIsClicked] = useState(false);
 
-    const addTodo = () => {
-        
+    const hanleClick = () => {
+        if (!isClicked) {
+            setIsClicked(true);
+        } else {
+            setIsClicked(false);
+        }
     }
 
     return(
-        <div className="flex justify-center pt-5">
-            <a href="#" type="button" className="text-3xl px-10 text-center rounded-full text-white bg-slate-500 hover:scale-150 transition-all hover:text-black hover:bg-orange-600">+</a>
+        <div className="flex flex-col items-center pt-5">
+            {isClicked && <div>
+                            <input type="text" placeholder="Escriba alguna tarea"/>
+                            <button>Agregar</button>
+                          </div>
+            }
+            <button onClick={hanleClick} type="button" className="text-3xl px-10 text-center rounded-full text-white bg-slate-500 hover:scale-110 transition-all hover:text-black hover:bg-orange-600">+</button>
         </div>
     )
 }
