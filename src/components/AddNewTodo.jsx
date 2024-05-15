b53c3ef70247308fc9a3ee7e16403d751720c83f
PITT1 <petroleo.plta.97@gmail.com>
2024-05-15 09:52:37 -0400

    estilando un poco

--- src/components/AddNewTodo.jsx
+++ src/components/AddNewTodo.jsx
@@ -13,9 +13,9 @@ const AddNewTodo = () => {
 
     return(
         <div className="flex flex-col items-center pt-5">
-            {isClicked && <div>
-                            <input type="text" placeholder="Escriba alguna tarea"/>
-                            <button>Agregar</button>
+            {isClicked && <div className="bg-slate-700 rounded-full my-3">
+                            <input type="text" placeholder="Escriba alguna tarea" className="text-2xl py-3 px-6 bg-transparent outline-none text-white"/>
+                            <button className="text-2xl py-3 px-3 rounded-full bg-orange-600 hover:bg-orange-500 transition-all">Agregar</button>
                           </div>
             }
             <button onClick={hanleClick} type="button" className="text-3xl px-10 text-center rounded-full text-white bg-slate-500 hover:scale-110 transition-all hover:text-black hover:bg-orange-600">+</button>
