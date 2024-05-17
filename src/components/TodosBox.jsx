6f59d89792cc3d6de577567760cd9d38cf356ff9
PITT1 <petroleo.plta.97@gmail.com>
2024-05-17 10:43:28 -0400

    logica del check todo casi lista

--- src/components/TodosBox.jsx
+++ src/components/TodosBox.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import AddNewTodo from "./AddNewTodo";
 import DeleteTodo from "./DeleteTodo";
 
-
 const TodoBox = () => {
   const [todoList, setTodoList] = useState([]);
   const [update, setUpdate] = useState(1);
@@ -11,34 +10,52 @@ const TodoBox = () => {
     setUpdate(prev => prev + 1);
   }
 
+  const finishedTodo = (taskIndex) => {
+    let updatedTodoList = [...todoList];
+    updatedTodoList[taskIndex].listo =!updatedTodoList[taskIndex].listo;
+    setTodoList(updatedTodoList);
+  };
+
   useEffect(() => {
     const urlString = window.location.href;
     const url = new URL(urlString);
     const user = url.searchParams.get("user");
 
     fetch(`http://localhost/myapi/my-api.php/todos/${user}`)
-      .then((res) => res.json())
-      .then((todos) => {
-        setTodoList(todos.map((tareas) => tareas.contenido));
+   .then((res) => res.json())
+   .then((todos) => {
+        const initialTodos = todos.map(tarea => ({
+         ...tarea,
+          listo: tarea.listo === 'true'
+        }));
+        
+        setTodoList(initialTodos);
       });
   }, [update]);
 
   return (
     <>
-    <div className="flex justify-center">
-      <h1 className="text-white text-center bg-slate-700 cursor-default w-2/5 rounded-b-full text-2xl font-medium">Tu lista de tareas</h1>
-    </div>
+      <div className="flex justify-center">
+        <h1 className="text-white text-center bg-slate-700 cursor-default w-2/5 rounded-b-full text-2xl font-medium">
+          Tu lista de tareas
+        </h1>
+      </div>
       <ul className="flex flex-col items-center">
         {todoList.map((tarea, index) => (
-          <li
-            key={index}
-            className="text-slate-800 font-semibold text-2xl pl-8 my-2 bg-slate-500 rounded-full cursor-default hover:scale-110 transition-all hover:bg-slate-100"
-          >
-            {tarea}<DeleteTodo keyIndex={index} onUpdate={isUpdate}/>
-          </li>
+          <div className="flex">
+            {tarea.listo && <div className="text-white h-8 rounded-full px-8 bg-green-600 text-2xl">✔</div>}
+            <li
+              key={index}
+              className="text-slate-800 font-semibold text-2xl pl-8 my-2 bg-slate-500 rounded-full cursor-pointer hover:scale-110 transition-all hover:bg-slate-100"
+              onClick={() => finishedTodo(index)}
+            >
+              {tarea.contenido}
+              <DeleteTodo keyIndex={index} onUpdate={isUpdate} />
+            </li>
+          </div>
         ))}
       </ul>
-      <AddNewTodo onUpdate={isUpdate}/>
+      <AddNewTodo onUpdate={isUpdate} />
     </>
   );
 };
